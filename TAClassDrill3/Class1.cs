31bad5438e0682075f36cd093906fba695fe090d
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-23 18:25:23 -0400

    Class with optional variable
    don't forget i can use empty strings

---
+++ TAClassDrill3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAClassDrill3
+{
+    class Numbers
+    {
+        public void Math(int num1, int num2 = 0)
+        {            
+            int result = num1 + num2;
+            Console.WriteLine("Your numbers added is : {0}", result);                      
+        }
+    }
+}
--- TAClassDrill3/Program.cs
+++ TAClassDrill3/Program.cs
@@ -10,6 +10,26 @@ namespace TAClassDrill3
     {
         static void Main(string[] args)
         {
+            int num1;
+            string userInput;
+
+            Console.WriteLine("Give me a number.");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me a second number if you want to.");
+            userInput = Console.ReadLine();
+
+            if (userInput == "")
+            {
+                Numbers math = new Numbers();
+                math.Math(num1);
+            }
+            else
+            {
+                Numbers math2 = new Numbers();
+                math2.Math(num1, Convert.ToInt32(userInput));
+            }
+            Console.ReadLine();
         }
     }
 }
